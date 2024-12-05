dd07461842ef4ca707e379fd940e1b830faf91d7
Francis Xavier Irungaray <fran-irunga@hotmail.com>
2024-12-05 14:26:15 -0300

    agregando angular

---
+++ src/MySeries.EntityFrameworkCore/Migrations/20241205172017_series.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MySeries.Migrations
+{
+    /// <inheritdoc />
+    public partial class series : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Appseries",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Appseries");
+        }
+    }
+}
