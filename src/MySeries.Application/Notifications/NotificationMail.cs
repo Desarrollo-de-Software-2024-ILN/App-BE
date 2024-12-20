7985711a90fed9d372a4d5c400de9cd4360a2ec6
Dardo Noir <noird@o365frcuutneduar.onmicrosoft.com>
2024-12-20 15:16:37 -0300

    .

---
+++ src/MySeries.Application/Notifications/NotificationMail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySeries.Notifications
+{
+    public class NotificatorMail : INotificator
+    {
+        public bool PuedeEnviar(TipoNotificacion tipo)
+        {
+            return tipo == TipoNotificacion.Mail;
+        }
+
+        public async Task EnvioNotificationsAsync(NotificationDto notificationDto)
+        {
+            var notification = new Notification
+            {
+                UsuarioID = notificationDto.UsuarioID,
+                Titulo = notificationDto.Titulo,
+                Msj = notificationDto.Msj,
+                Leida = false,
+                Tipo = notificationDto.Tipo,
+            };
+
+            Console.WriteLine($"Email enviado a {notification.UsuarioID}: {notification.Titulo} - {notification.Msj}");
+        }
+    }
+}
